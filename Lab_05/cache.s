cb12b6ba5c4784719717fb29364151afb8d9e254
Muhammad Usman <uthmanhere@gmail.com>
2019-09-08 04:07:53 +0500

    Update cache.s

--- Lab_05/cache.s
+++ Lab_05/cache.s
@@ -3,16 +3,27 @@ array:	.word	2048
 
 .text
 
-# change the following parameters: ####
-	li		a0,	512		# size of array
-	li		a1,	16		# step size
-#######################################
-	la		t1, array
-	add		t3,t1,a0
+## change the following parameters: ########################
+	li		a0,16		# number of accesses
+	li		a1,64		# step size
+	li		a2,4		# rep count
+	li		a3,1		# offset
+############################################################
+
+	li		t3,0
+rep_loop:
+	addi	t4,a0,0
+	la		t1,array
+	add		t1,t1,t3
 array_loop:
 	lb		t2,0(t1)
 	add		t1,t1,a1
-	blt 	t1,t3,array_loop
-exit:    
-	li	a0,10
+	addi	t4,t4,-1
+	bne 	t4,x0,array_loop
+	add		t3,t3,a3
+	bne		t3,a2,rep_loop
+
+exit:
+	li		a0,10
 	ecall
+
