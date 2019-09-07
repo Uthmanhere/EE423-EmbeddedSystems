329fce1b13c41348150789b5fee78fc00dd964c9
Muhammad Usman <uthmanhere@gmail.com>
2019-09-07 16:20:45 +0500

    Update cache.s

--- Lab_05/cache.s
+++ Lab_05/cache.s
@@ -5,14 +5,14 @@ array:	.word	2048
 
 # change the following parameters: ####
 	li		a0,	512		# size of array
-    li		a1,	16		# step size
+	li		a1,	16		# step size
 #######################################
-    la		t1, array
-    add		t3,t1,a0
+	la		t1, array
+	add		t3,t1,a0
 array_loop:
-    lb		t2,0(t1)
+	lb		t2,0(t1)
 	add		t1,t1,a1
 	blt 	t1,t3,array_loop
 exit:    
-    li	a0,10
-    ecall
+	li	a0,10
+	ecall
