be47e5251b199482361f249716e8041b356c5133
Muhammad Usman <uthmanhere@gmail.com>
2019-09-08 05:14:35 +0500

    Update cache.s

--- Lab_05/cache.s
+++ Lab_05/cache.s
@@ -3,11 +3,11 @@ array:	.word	2048
 
 .text
 
-## change the following parameters: ########################
-	li		a0,16		# number of accesses
+# change the following parameters: #########################
+	li		a0,4		# number of accesses
 	li		a1,64		# step size
-	li		a2,4		# rep count
-	li		a3,1		# offset
+	li		a2,32		# rep count
+	li		a3,2		# offset
 ############################################################
 
 	li		t3,0
@@ -21,9 +21,9 @@ array_loop:
 	addi	t4,t4,-1
 	bne 	t4,x0,array_loop
 	add		t3,t3,a3
-	bne		t3,a2,rep_loop
+	addi	t5,t5,1
+	bne		t5,a2,rep_loop
 
 exit:
 	li		a0,10
 	ecall
-
