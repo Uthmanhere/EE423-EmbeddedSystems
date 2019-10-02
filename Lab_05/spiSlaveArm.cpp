b2c6bc5c57b30b4020efb079322dff261e8ebd84
Muhammad Usman <uthmanhere@gmail.com>
2019-10-02 03:03:46 -0700

    Update spiSlaveArm.cpp

--- Lab_05/spiSlaveArm.cpp
+++ Lab_05/spiSlaveArm.cpp
@@ -1,1 +1,13 @@
+#include "mbed.h"
 
+SPISlave spi_slave(PA_7, PA_6, PA_5, PA_4);  // mosi, miso, sclk, ssel
+
+int main() {    
+   spi_slave.reply(0x00);  //first reply
+   while(1) {
+       if(spi_slave.receive()) {
+           int v = spi_slave.read();  // Read byte from master
+           printf("Value read is %d.\n\r", v);
+       }
+   }
+}
