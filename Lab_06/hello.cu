1b85060172596edb731180330f0614b0a08043c0
Muhammad Usman <uthmanhere@gmail.com>
2019-10-09 21:14:56 +0500

    Update hello.cu

--- Lab_06/hello.cu
+++ Lab_06/hello.cu
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+// kernel function
 __global__ void cuda_hello()
 {
+        // nothing prints as such functionalities
+        // are not provided by device
         printf("Hello world from GPU\n");
 }
 
 int main()
 {
+        // calling the kernel
         cuda_hello<<<1,1>>>();
         return 0;
 }
