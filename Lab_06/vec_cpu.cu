6ed69a3ca7cb9f0f00dbd0a1fd4881446ebaf850
Muhammad Usman <uthmanhere@gmail.com>
2019-10-09 21:19:07 +0500

    Update vec_cpu.cu

--- Lab_06/vec_cpu.cu
+++ Lab_06/vec_cpu.cu
@@ -1,25 +1,30 @@
-#define N 10000
+#define N 10000  // vector size
 
 void compute( float * a, float * b, float * c )
 {
+        // compute tangent for each number
         for (int i=0; i<N; i++)
                 c[i] = b[i] / a[i];
 }
 
 int main(void)
 {
+        // initialize vectors
         float a[N], b[N], c[N];
 
+        // define numbers as sines and cosines
         for (int i=0; i<N; i++)
         {
                 a[i] = cos(i);
                 b[i] = sin(i);
         }
-
+        
+        // compute tangent
         compute(a, b, c);
 
+        // prints first 10 results
         for (int i=0; i<10; i++)
-                printf(">> for i %d, computes %f.\n", i, c[i]);
+                printf(" >> tangent of %d computes as %f.\n", i, c[i]);
 
 
 
